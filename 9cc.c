cb1c6fd23496ef5a6250787768f7172ffae375e5
Kento KAWAKAMI <emaxser@bonprosoft.com>
2018-11-11 11:15:20 +0900

    add tokenizer

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,57 @@
+#include <ctype.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+enum {
+	TK_NUM = 256,
+	TK_EOF,
+};
+
+typedef struct{
+	int ty;
+	int val;
+	char *input;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p){
+	int i = 0;
+	while (*p) {
+		if (isspace(*p)) {
+			p++;
+			continue;
+		}
+
+		if (*p == '+' || *p == '-') {
+			tokens[i].ty = *p;
+			tokens[i].input = p;
+			i++;
+			p++;
+			continue;
+		}
+
+		if (isdigit(*p)) {
+			tokens[i].ty = TK_NUM;
+			tokens[i].input = p;
+			tokens[i].val = strtol(p, &p, 10);
+			i++;
+			continue;
+		}
+
+		fprintf(stderr, "fail tokenaze: %s\n",p);
+		exit(1);
+	}
+	tokens[i].ty = TK_EOF;
+	tokens[i].input = p;
+}
+
+void error(int i){
+	fprintf(stderr, "unexpeted token: %s\n", tokens[i].input);
+	exit(1);
+}
+
 
 int main(int argc, char **argv){
 	if(argc != 2){
@@ -7,28 +59,40 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
-
-	char *p = argv[1];
+	tokenize(argv[1]);
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
-	printf("\tmov rax, %ld\n", strtol(p, &p, 10));
 
-	while (*p) {
-		if(*p == '+'){
-			p++;
-			printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+	if(tokens[0].ty != TK_NUM){
+		error(0);
+	}
+
+	printf("\tmov rax, %d\n", tokens[0].val);
+
+	int i = 1;
+	while (tokens[i].ty != TK_EOF) {
+		if (tokens[i].ty == '+') {
+			i++;
+			if (tokens[i].ty != TK_NUM) {
+				error(i);
+			}
+			printf("\tadd rax, %d\n", tokens[i].val);
+			i++;
+			continue;
+		}
+		if (tokens[i].ty == '-') {
+			i++;
+			if (tokens[i].ty != TK_NUM) {
+				error(i);
+			}
+			printf("\tsub rax, %d\n", tokens[i].val);
+			i++;
 			continue;
 		}
 
-		if(*p == '-'){
-			p++;
-			printf("\tsub rax, %ld\n", strtol(p, &p, 10));
-			continue;
-		}
-
-		fprintf(stderr, "invalide chara: '%c'\n", *p);
+		error(i);
 	}
 
 	printf("\tret\n");
